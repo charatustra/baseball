108421768412456be6aaab0966ca4d547a1888ff
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-13 13:50:30 +0900

    [feature] page 1

--- test_game.py
+++ test_game.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
 
+from game import Game
+
 
 class TestGame(TestCase):
-    def test_game(self):
-        self.assertEqual(1,1)
-        pass
+    def test_exception_when_input_is_none(self):
+        self.game = Game()
+        with self.assertRaises(TypeError):
+            self.game.guess(None)
